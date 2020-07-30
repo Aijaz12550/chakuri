51540093754183b86cb22872c8c0dbf32c5f9fc6
mowaiswarsi <muhammadowaiswarsi@gmail.com>
2020-07-30 12:43:30 +0500

    header and footer ui completed

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
 
 export const Footer = () => {
-  return(
-    <div>
-      This is footer
+  return (
+    <div className="footer-container">
+      <div className="footer-child-1">
+        <div className="footer-child-2 text-container">
+          <h3>This is Heading:</h3>
+          <p> hello from footer</p>
+          <p> hello from footer</p>
+          <p> hello from footer</p>
+          <p> hello from footer</p>
+          <p> hello from footer</p>
+        </div>
+
+        <div className="footer-child-2 text-container">
+          <h3>This is Heading:</h3>
+          <p> hello from footer</p>
+          <p> hello from footer</p>
+          <p> hello from footer</p>
+          <p> hello from footer</p>
+          <p> hello from footer</p>
+        </div>
+
+        <div className="footer-child-2 text-container">
+          <h3>This is Heading:</h3>
+          <p> hello from footer</p>
+          <p> hello from footer</p>
+          <p> hello from footer</p>
+          <p> hello from footer</p>
+          <p> hello from footer</p>
+        </div>
+
+        <div className="footer-child-2 text-container">
+          <h3>This is Heading:</h3>
+          <p> hello from footer</p>
+          <p> hello from footer</p>
+          <p> hello from footer</p>
+          <p> hello from footer</p>
+          <p> hello from footer</p>
+        </div>
+
+      </div>
+      <div className="footer-child-1">
+      <h4>
+        Get it on your mobile phone from <h2>Play Store</h2> or <h2>App Store</h2>
+      </h4>
+      </div>
+        
     </div>
-  )
-}
+  );
+};
--- src/components/signin/index.jsx
+++ src/components/signin/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Signin = () => {
   return(
-    <div>
+    <div className='signin-container'>
       <input type='text' />
         <input type='text' />
         <button>SignIn</button>
