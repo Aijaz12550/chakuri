6b013a4f398b5544e5e14084b03993feb1d059bc
Aijaz Abbasi <muhammadaijaz76@gmail.com>
2020-07-31 19:21:36 +0500

    export error fix 01

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="header-container">
+      <span className="header-sub-container min-width-300">
+        <span>Search Jobs</span>
+        <span>About Us</span>
+        <span>Contact Us</span>
+      </span>
+      <span className="header-sub-container"></span>
+    </div>
+  );
+};
